93f4ba0a10fafd8178c3f54263bbc4eafbbbd9cf
Bagasbelajarhtml <145095794+Bagasbelajarhtml@users.noreply.github.com>
2024-07-05 19:08:27 +0700

    first commit

---
+++ lib/screens/province_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rumahsakit/cubits/province/province_cubit.dart';
+import 'package:rumahsakit/screens/city_screen.dart';
+
+class ProvinceScreen extends StatefulWidget {
+  const ProvinceScreen({super.key});
+
+  @override
+  State<ProvinceScreen> createState() => _ProvinceScreenState();
+}
+
+class _ProvinceScreenState extends State<ProvinceScreen> {
+  ProvinceCubit _provinceCubit = ProvinceCubit();
+
+  @override
+  void initState() {
+    _provinceCubit.LoadDataProvince();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: BoxDecoration(color: Colors.blue),
+        ),
+        title: Center(child: const Text('Ini Daftar Provinsi')),
+      ),
+      body: BlocBuilder<ProvinceCubit, ProvinceState>(
+        bloc: _provinceCubit,
+        builder: (context, state) {
+          if (state is ProvinceData) {
+            return ListView.builder(
+              itemCount: state.data.provinces?.length ?? 0,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => CityScreen(
+                          province: state.data.provinces?[index].id ?? "",
+                          proviceName: state.data.provinces?[index].name ?? "",
+                        ),
+                      ),
+                    );
+                  },
+                  title: Text(state.data.provinces?[index].name ?? ""),
+                );
+              },
+            );
+          }
+          return const Center(
+            child: Text('Loading Data'),
+          );
+        },
+      ),
+    );
+  }
+}
